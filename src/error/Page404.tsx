00d55b3f651be3ac0e3691bab738f9830f980aae
arif-hossaian <arifhossain42136@gmail.com>
2022-01-27 18:22:23 +0600

    some modification

--- src/error/Page404.tsx
+++ src/error/Page404.tsx
@@ -6,7 +6,7 @@ const Page404: React.FC = () => {
   return (
     <div className="nc-Page404">
       <Helmet>
-        <title>404 | TicketsForTravel</title>
+        <title>404 | Show Demo data</title>
       </Helmet>
       <div className="container relative pt-5 pb-16 lg:pb-20 lg:pt-5">
         <header className="text-center max-w-2xl mx-auto space-y-2 my-24">
